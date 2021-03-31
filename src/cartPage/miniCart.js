d95d05ca32d9d1e9777700786d2cfdf41fbab15b
Varun Manoharan <varun.manoharan@publicissapient.com>
2021-03-31 15:33:53 +0530

    mini cart functionality for products page

--- src/cartPage/miniCart.js
+++ src/cartPage/miniCart.js
@@ -1,19 +1,25 @@
 let miniCart = document.getElementById('mini-cart');
+let miniCartSection = document.getElementById('mini-cart-section')
 let modalBackground = document.getElementById('modal-background');
+let closeBtn = document.querySelector('a.close-btn');
 
 if(modalBackground) {
     modalBackground.addEventListener('click', close);
+}
+
+if(closeBtn) {
+    closeBtn.addEventListener('click', close);
 }
 
 function loadCart() {
     
     modalBackground.style.display = 'block';
    
-    miniCart.style.display = 'flex';
-    miniCart.innerHTML = '<object style="width:100%;" type="text/html" data="../../cartPage/miniCart.html" ></object>';
+    miniCartSection.style.display = 'block';
+    miniCart.innerHTML = '<object style="width:100%;height:100%" type="text/html" data="../../cartPage/miniCart.html" ></object>';
 }
 
 function close() {
-    miniCart.style.display = 'none';
+    miniCartSection.style.display = 'none';
     modalBackground.style.display = 'none';
 }
