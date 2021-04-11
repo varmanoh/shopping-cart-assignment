a7cd392bb35841a28cc17536ff2aeffc2c3fbf04
Varun Manoharan <varun.manoharan@publicissapient.com>
2021-04-11 18:11:19 +0530

    new all products page and home page changes

--- src/productsPage/cardDetails.js
+++ src/productsPage/cardDetails.js
@@ -1,14 +1,14 @@
 export const cardDetails = (card) => `
-    <div class="cards">
+    <div class="card">
         <div class="card-heading">
             <h2>${card.name}</h2>
         </div>
         <div class="card-contents d-flex">
             <img src="../../..${card.imageURL}" alt="fruits" class="product-img">
             <div class="card-details">
-                <div class="card-text">
+                <p class="card-text">
                     ${card.description}
-                </div>
+                </p>
                 <button class="app-btn buy-btn">Buy Now @ Rs.${card.price}</button>
                 <div class="buy-button-desktop">
                     <span class="rate">MRP Rs.${card.price}</span>
